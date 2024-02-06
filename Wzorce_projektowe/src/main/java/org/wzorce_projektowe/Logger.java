54c120b7c2ec4ce8439dd95754a818f7f30ea39a
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-02-06 23:58:47 +0100

    Singleton - jak go implementujemy?

--- Wzorce_projektowe/src/main/java/org/wzorce_projektowe/Logger.java
+++ Wzorce_projektowe/src/main/java/org/wzorce_projektowe/Logger.java
@@ -10,12 +10,12 @@ public class Logger {
     private Logger(){}
 
     public static Logger getInstance(){
-        if (Logger.instance==null){
-            instance = new Logger();
-        }
-        return instance;
+        return SingletonHolder.INSTANCE;
     }
     public void logToConsole(){
 //
+    }
+    private static class SingletonHolder{
+        private static final Logger INSTANCE = new Logger();
     }
 }
