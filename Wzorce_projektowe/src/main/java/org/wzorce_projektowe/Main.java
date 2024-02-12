e0d49b5401d1fa2f0359eb20f2c03fc0a2ea2abc
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-02-12 23:30:25 +0100

    Wzorzec dekorator - jak go używamy?

--- Wzorce_projektowe/src/main/java/org/wzorce_projektowe/Main.java
+++ Wzorce_projektowe/src/main/java/org/wzorce_projektowe/Main.java
@@ -16,6 +16,7 @@ public class Main {
 //        observerPattern();
 
         Employee mike = new Employee();
+        mike.setSalary(10000);
 
         mike.travelStrategy = new BikeTravelStrategy();
         mike.jobStrategy = new DoctorJobStrategy();
@@ -33,6 +34,14 @@ public class Main {
 
         System.out.println("zarobki " + mike.getSalary());
         System.out.println("zarobki " + new FreqBonus(new DeadlineBonus(new SpecialBonus(mike))).getSalary());
+
+        Payable employee = mike;
+        if (mike.getSalary()>8000){
+            employee = new SpecialBonus(employee);
+        }
+        if (mike.travelStrategy instanceof BikeTravelStrategy){
+            employee = new FreqBonus(employee);
+        }
 
     }
 
--- Wzorce_projektowe/src/main/java/org/wzorce_projektowe/strategy/Employee.java
+++ Wzorce_projektowe/src/main/java/org/wzorce_projektowe/strategy/Employee.java
@@ -1,10 +1,16 @@
 package org.wzorce_projektowe.strategy;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Adam Seweryn
  */
 public class Employee implements TravelStrategy, JobStrategy, BreakfastStrategy {
 
+    @Getter
+    @Setter
+    private int salary = 0;
     public TravelStrategy travelStrategy;
     public JobStrategy jobStrategy;
     public BreakfastStrategy breakfastStrategy;
